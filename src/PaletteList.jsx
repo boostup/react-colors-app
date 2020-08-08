308cf6220c63114ef49af2c3ccd8ab1015870f7d
boostup <boostup@gmail.com>
2020-08-08 16:45:02 +0200

    styles homepage minipalettes

--- src/PaletteList.jsx
+++ src/PaletteList.jsx
@@ -1,19 +1,57 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { withStyles } from "@material-ui/styles";
 import MiniPalette from "./MiniPalette";
 
 function PaletteList(props) {
-  const { palettes } = props;
+  const { palettes, classes } = props;
+
   return (
-    <div>
-      <MiniPalette />
-      <h1>React Color Palettes</h1>
-      {palettes.map((palette) => (
-        <p>
-          <Link to={`/palette/${palette.id}`}>{palette.paletteName}</Link>
-        </p>
-      ))}
+    <div className={classes.root}>
+      <div className={classes.container}>
+        <nav className={classes.nav}>
+          <h1>React Colors</h1>
+        </nav>
+        <div className={classes.palettes}>
+          {palettes.map((palette) => (
+            <Link key={palette.paletteName} to={`/palette/${palette.id}`}>
+              <MiniPalette {...palette} />
+            </Link>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
-export default PaletteList;
+
+const styles = {
+  root: {
+    backgroundColor: "blue",
+    height: "100vh",
+    display: "flex",
+    alignItems: "flex-start",
+    justifyContent: "center",
+  },
+  container: {
+    width: "50%",
+    display: "flex",
+    alignItems: "flex-start",
+    flexDirection: "column",
+    flexWrap: "wrap",
+  },
+  nav: {
+    height: "100%",
+    display: "flex",
+    justifyContent: "center",
+    color: "white",
+  },
+  palettes: {
+    boxSizing: "border-box",
+    width: "100%",
+    display: "grid",
+    gridTemplateColumns: "repeat(3,30%)",
+    gridGap: "5%",
+  },
+};
+
+export default withStyles(styles)(PaletteList);
