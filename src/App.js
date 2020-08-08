1858effede86ef1ae97413ef46e484a3e967c731
boostup <boostup@gmail.com>
2020-08-08 14:22:50 +0200

    added router and set initial routes

--- src/App.js
+++ src/App.js
@@ -3,12 +3,27 @@ import seedPalettes from "./seedPalettes";
 import Palette from "./Palette";
 import { generatePalette } from "./colorHelpers";
 import "./App.css";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
+  const findPalette = (id) => seedPalettes.find((palette) => palette.id === id);
+
   return (
-    <div className="App">
-      <Palette palette={generatePalette(seedPalettes[4])} />
-    </div>
+    <Switch>
+      <Route exact path="/" render={() => <h1>Home</h1>} />
+      <Route
+        exact
+        path="/palette/:id"
+        render={(routeProps) => (
+          <Palette
+            palette={generatePalette(findPalette(routeProps.match.params.id))}
+          />
+        )}
+      />
+    </Switch>
+    // <div className="App">
+    //   <Palette palette={generatePalette(seedPalettes[4])} />
+    // </div>
   );
 }
 
