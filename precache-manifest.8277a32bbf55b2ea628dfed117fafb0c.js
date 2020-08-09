2764b71afb2431d2b256abcbe093d583e9409da3
boostup <boostup@gmail.com>
2020-08-09 09:17:50 +0200

    Updates

---
+++ precache-manifest.8277a32bbf55b2ea628dfed117fafb0c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e91b4040a917991034e41e049ae60a1",
+    "url": "/react-colors-app/index.html"
+  },
+  {
+    "revision": "a50c2d86cf8bfd69dd89",
+    "url": "/react-colors-app/static/css/2.ed590494.chunk.css"
+  },
+  {
+    "revision": "4ae6a4db7f4a8b99a805",
+    "url": "/react-colors-app/static/css/main.09c2287e.chunk.css"
+  },
+  {
+    "revision": "a50c2d86cf8bfd69dd89",
+    "url": "/react-colors-app/static/js/2.a80d274a.chunk.js"
+  },
+  {
+    "revision": "fe2f19c5d5965d51615a596cf8d07fe9",
+    "url": "/react-colors-app/static/js/2.a80d274a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ae6a4db7f4a8b99a805",
+    "url": "/react-colors-app/static/js/main.8fc0e72d.chunk.js"
+  },
+  {
+    "revision": "02c02241342ce7eec765",
+    "url": "/react-colors-app/static/js/runtime-main.5cd1b448.js"
+  }
+]);
